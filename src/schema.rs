37fa22cabb20cf1eb14a22b4b776bbcfe6959c80
senderro <132297098+senderro@users.noreply.github.com>
2025-02-11 13:16:22 -0300

    att

--- src/schema.rs
+++ src/schema.rs
@@ -1,21 +1,4 @@
 // @generated automatically by Diesel CLI.
-
-diesel::table! {
-    artistas (id) {
-        id -> Int4,
-        #[max_length = 255]
-        nome -> Varchar,
-    }
-}
-
-diesel::table! {
-    imagemartista (id) {
-        id -> Int4,
-        iddoartista -> Nullable<Int4>,
-        imagem -> Text,
-    }
-}
-
 diesel::table! {
     todo (id) {
         id -> Int4,
@@ -29,11 +12,3 @@ diesel::table! {
         due_date -> Nullable<Timestamp>,
     }
 }
-
-diesel::joinable!(imagemartista -> artistas (iddoartista));
-
-diesel::allow_tables_to_appear_in_same_query!(
-    artistas,
-    imagemartista,
-    todo,
-);
